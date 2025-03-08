5a5a984e5c87ae75225ef951a6bd6c04b7962010
matheusxd8147 <matheusmonteiro8147@gmail.com>
2025-03-08 18:16:17 -0300

    Aualização 21L1 diagnosticar problema 3

--- examples/VIED_21L1/server_example_goose.c
+++ examples/VIED_21L1/server_example_goose.c
@@ -1134,6 +1134,7 @@ main(int argc, char** argv)
     while (running) {
 
         IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_ANN_MVGGIO12_AnIn01_mag_f, 221.00);
+        IedServer_updateBooleanAttributeValue(iedServer, IEDMODEL_CON_RBGGIO1_SPCSO02_stVal, true);
         IedServer_updateBooleanAttributeValue(iedServer, IEDMODEL_ANN_INAGGIO1_Ind01_stVal, true);
         Thread_sleep(17);
 
