7987ab4a0204aae15f4de4e466edad80e3745da9
matheusxd8147 <matheusmonteiro8147@gmail.com>
2025-07-07 10:06:30 -0300

    Teste novo 43

--- examples/VIED_21L8/server_example_goose.c
+++ examples/VIED_21L8/server_example_goose.c
@@ -1054,7 +1054,7 @@ gooseListener4(GooseSubscriber subscriber, void* parameter)
     comando_received_21l6_F = buffer[7];
 
     float tensao;
-    tensao = atof(&buffer[12]);
+    tensao = atof(&buffer[13]);
 
     system("clear");
     printf("\nTESTE NOVA MENSAGEM CONTROLE RELIGAMENTO\nCOMANDO 1: %d\nCOMANDO 2: %d\nTENSÂO V = %f\n", comando_received_21l6_A, comando_received_21l6_F, tensao);
